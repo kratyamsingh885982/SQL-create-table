00cc3a828ff637bc80e58e84476ecb5fb30dfa73
kratyam singh <karankushwaha7194@gmail.com>
2025-04-26 11:53:54 +0530

    Add files via upload

---
+++ create table&database.sql
@@ -0,0 +1,10 @@
+-- create databsase;
+create database college;
+-- create table;
+create table college.student(
+s_id int,
+s_name varchar(100),
+s_age int,
+s_course varchar(100),
+s_rollno int
+);
